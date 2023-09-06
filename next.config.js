3ba637fdecfb1cc33e776a586eead0adfefc3e06
javier <javier@wisboo.com>
2023-09-06 19:16:35 -0300

    next deploy

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    async generateStaticParams() {
+        const paths = {
+            '/': {page: '/'}
+        }
+    },
+    output: 'export'
+}
 
 module.exports = nextConfig
